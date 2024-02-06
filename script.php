1e3c371e4768e6a6885dbc06368ea902c8973b69
Sergey Tolkachyov <musstudent@mail.ru>
2024-02-06 18:09:21 +0400

    - косметика и code style - правка языковых констант - сообщение post install script

--- script.php
+++ script.php
@@ -22,157 +22,161 @@ use Joomla\DI\ServiceProviderInterface;
 
 return new class () implements ServiceProviderInterface {
 
-    public function register(Container $container): void
-    {
-        $container->set(InstallerScriptInterface::class, new class ($container->get(AdministratorApplication::class)) implements InstallerScriptInterface {
-            /**
-             * The application object
-             *
-             * @var  AdministratorApplication
-             *
-             * @since  1.0.0
-             */
-            protected AdministratorApplication $app;
+	public function register(Container $container): void
+	{
+		$container->set(InstallerScriptInterface::class, new class ($container->get(AdministratorApplication::class)) implements InstallerScriptInterface {
+			/**
+			 * The application object
+			 *
+			 * @var  AdministratorApplication
+			 *
+			 * @since  1.0.0
+			 */
+			protected AdministratorApplication $app;
 
-            /**
-             * The Database object.
-             *
-             * @var   DatabaseDriver
-             *
-             * @since  1.0.0
-             */
-            protected DatabaseDriver $db;
+			/**
+			 * The Database object.
+			 *
+			 * @var   DatabaseDriver
+			 *
+			 * @since  1.0.0
+			 */
+			protected DatabaseDriver $db;
 
-            /**
-             * Minimum Joomla version required to install the extension.
-             *
-             * @var  string
-             *
-             * @since  1.0.0
-             */
-            protected string $minimumJoomla = '4.3';
+			/**
+			 * Minimum Joomla version required to install the extension.
+			 *
+			 * @var  string
+			 *
+			 * @since  1.0.0
+			 */
+			protected string $minimumJoomla = '4.3';
 
-            /**
-             * Minimum PHP version required to install the extension.
-             *
-             * @var  string
-             *
-             * @since  1.0.0
-             */
-            protected string $minimumPhp = '7.4';
+			/**
+			 * Minimum PHP version required to install the extension.
+			 *
+			 * @var  string
+			 *
+			 * @since  1.0.0
+			 */
+			protected string $minimumPhp = '7.4';
 
-            /**
-             * @var array $providersInstallationMessageQueue
-             * @since 2.0.3
-             */
-            protected $providersInstallationMessageQueue = [];
+			/**
+			 * @var array $providersInstallationMessageQueue
+			 * @since 2.0.3
+			 */
+			protected $providersInstallationMessageQueue = [];
 
-            /**
-             * Constructor.
-             *
-             * @param AdministratorApplication $app The application object.
-             *
-             * @since 1.0.0
-             */
-            public function __construct(AdministratorApplication $app)
-            {
-                $this->app = $app;
-                $this->db = Factory::getContainer()->get('DatabaseDriver');
-            }
+			/**
+			 * Constructor.
+			 *
+			 * @param   AdministratorApplication  $app  The application object.
+			 *
+			 * @since 1.0.0
+			 */
+			public function __construct(AdministratorApplication $app)
+			{
+				$this->app = $app;
+				$this->db  = Factory::getContainer()->get('DatabaseDriver');
+			}
 
-            /**
-             * Function called after the extension is installed.
-             *
-             * @param InstallerAdapter $adapter The adapter calling this method
-             *
-             * @return  boolean  True on success
-             *
-             * @since   1.0.0
-             */
-            public function install(InstallerAdapter $adapter): bool
-            {
-                return true;
-            }
+			/**
+			 * Function called after the extension is installed.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   1.0.0
+			 */
+			public function install(InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
 
-            /**
-             * Function called after the extension is updated.
-             *
-             * @param InstallerAdapter $adapter The adapter calling this method
-             *
-             * @return  boolean  True on success
-             *
-             * @since   1.0.0
-             */
-            public function update(InstallerAdapter $adapter): bool
-            {
-                return true;
-            }
+			/**
+			 * Function called after the extension is updated.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   1.0.0
+			 */
+			public function update(InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
 
-            /**
-             * Function called after the extension is uninstalled.
-             *
-             * @param InstallerAdapter $adapter The adapter calling this method
-             *
-             * @return  boolean  True on success
-             *
-             * @since   1.0.0
-             */
-            public function uninstall(InstallerAdapter $adapter): bool
-            {
-                return true;
-            }
+			/**
+			 * Function called after the extension is uninstalled.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   1.0.0
+			 */
+			public function uninstall(InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
 
-            /**
-             * Function called before extension installation/update/removal procedure commences.
-             *
-             * @param string $type The type of change (install or discover_install, update, uninstall)
-             * @param InstallerAdapter $adapter The adapter calling this method
-             *
-             * @return  boolean  True on success
-             *
-             * @since   1.0.0
-             */
-            public function preflight(string $type, InstallerAdapter $adapter): bool
-            {
-                return true;
-            }
+			/**
+			 * Function called before extension installation/update/removal procedure commences.
+			 *
+			 * @param   string            $type     The type of change (install or discover_install, update, uninstall)
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   1.0.0
+			 */
+			public function preflight(string $type, InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
 
-            /**
-             * Function called after extension installation/update/removal procedure commences.
-             *
-             * @param string $type The type of change (install or discover_install, update, uninstall)
-             * @param InstallerAdapter $adapter The adapter calling this method
-             *
-             * @return  boolean  True on success
-             *
-             * @since   1.0.0
-             */
-            public function postflight(string $type, InstallerAdapter $adapter): bool
-            {
-                $smile = '';
+			/**
+			 * Function called after extension installation/update/removal procedure commences.
+			 *
+			 * @param   string            $type     The type of change (install or discover_install, update, uninstall)
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   1.0.0
+			 */
+			public function postflight(string $type, InstallerAdapter $adapter): bool
+			{
+				$smile = '';
 
-                if ($type !== 'uninstall') {
-                    if ($type != 'uninstall') {
-                        $smiles = ['&#9786;', '&#128512;', '&#128521;', '&#128525;', '&#128526;', '&#128522;', '&#128591;'];
-                        $smile_key = array_rand($smiles, 1);
-                        $smile = $smiles[$smile_key];
-                    }
-                } else {
-                    $smile = ':(';
-                }
+				if ($type !== 'uninstall')
+				{
+					if ($type != 'uninstall')
+					{
+						$smiles    = ['&#9786;', '&#128512;', '&#128521;', '&#128525;', '&#128526;', '&#128522;', '&#128591;'];
+						$smile_key = array_rand($smiles, 1);
+						$smile     = $smiles[$smile_key];
+					}
+				}
+				else
+				{
+					$smile = ':(';
+				}
 
-                $element = 'PLG_' . strtoupper($adapter->getElement());
-                $type = strtoupper($type);
+				$element = 'PLG_' . strtoupper($adapter->getElement());
+				$type    = strtoupper($type);
 
-                $html = '
+				$html = '
 				<div class="row bg-white m-0">
 				<div class="col-12 col-md-8 p-0 pe-2">
 				<h2>' . $smile . ' ' . Text::_($element . '_AFTER_' . $type) . ' <br/>' . Text::_($element) . '</h2>
 				' . Text::_($element . '_DESC');
 
-                $html .= Text::_($element . '_WHATS_NEW');
+				$html .= Text::_($element . '_WHATS_NEW');
 
-                $html .= '</div>
+				$html .= '</div>
 				<div class="col-12 col-md-4 p-0 d-flex flex-column justify-content-start">
 				<img width="180" src="https://web-tolk.ru/web_tolk_logo_wide.png">
 				<p>Joomla Extensions</p>
@@ -184,10 +188,10 @@ return new class () implements ServiceProviderInterface {
 				' . Text::_($element . "_MAYBE_INTERESTING") . '
 				</div>
 				';
-                $this->app->enqueueMessage($html, 'info');
+				$this->app->enqueueMessage($html, 'info');
 
-                return true;
-            }
-        });
-    }
+				return true;
+			}
+		});
+	}
 };
